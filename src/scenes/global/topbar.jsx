fce207e6edea0052540c03afb8c9ec41ff921628
kinyua-davy <1805689@students.kcau.ac.ke>
2025-07-28 20:39:36 +0300

    restructure

---
+++ src/scenes/global/topbar.jsx
@@ -0,0 +1,4 @@
+const topbar = () =>{
+    return <div>topbar</div>
+}
+export default topbar;
---
+++ src/scenes/theme.js
@@ -0,0 +1,3 @@
+import { createContext,useMemo,useState } from "react";
+import { createTheme } from "@mui/material/styles";
+// color design
